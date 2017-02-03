5fd9e3ea507855ad3096a79f2e1b1dbfaa46645a
Vincent Hu <vincenthu@crescagroup.com>
2017-02-03 08:53:16 -0500

    Added admin to config

--- framework/controller/frameworkController.php
+++ framework/controller/frameworkController.php
@@ -6,11 +6,8 @@ class frameworkController extends controller {
     public function __construct()
     {
         parent::__construct();
-        if (!isset($_SERVER['PHP_AUTH_USER'])) {
-            header('WWW-Authenticate: Basic realm="My Realm"');
-            header('HTTP/1.0 401 Unauthorized');
-            exit ('You must log in to access this page.');
-        } else if ($_SERVER['PHP_AUTH_USER'] != '' || $_SERVER['PHP_AUTH_PW'] != '') {
+        if (!isset($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_USER'] != ADMINU || $_SERVER['PHP_AUTH_PW'] != ADMINP) {
+            header('WWW-Authenticate: Basic realm="Framework Controller"');
             header('HTTP/1.0 401 Unauthorized');
             exit ('Invalid login credentials.');
         }
