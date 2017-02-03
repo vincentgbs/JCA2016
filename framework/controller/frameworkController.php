14df7d307d0a5ed083405915d491644b0c1b76cf
Vincent Hu <vincenthu@crescagroup.com>
2017-02-03 11:25:17 -0500

    Working to add user permissions

--- framework/controller/frameworkController.php
+++ framework/controller/frameworkController.php
@@ -26,32 +26,33 @@ class frameworkController extends controller {
         if (isset($_POST['permission'])) {
             //
         }
-        $this->view .= '<div id="tabs"><ul><li><a href="#tabs-1">Permissions</a></li><li><a href="#tabs-2">Groups</a></li><li><a href="#tabs-3">Users</a></li><li><a href="#tabs-4">All</a></li></ul>';
-        $this->view .= '<div id="tabs-1">';
-        $ps = $this->select('SELECT * FROM `user_ls_permissions` `p`
-            LEFT JOIN `user_rel_permissions` `r` ON `p`.`permission_id`=`r`.`permission_id`
-            LEFT JOIN `user_ls_groups` `g` ON `r`.`group_id`=`g`.`group_id`;');
-        foreach ($ps as $p) {
-            $this->view .= serialize($p) . '<br>';
+        $this->view .= '<div id="tabs"><ul><li><a href="#tabs-4">All</a></li><li><a href="#tabs-1">Permissions</a></li><li><a href="#tabs-2">Groups</a></li><li><a href="#tabs-3">Users</a></li></ul>';
+        $this->view .= '<div id="tabs-1">
+        <table><thead><th>permission_id</th><th>function</th><th>group_id</th><th>group_name</th></thead><tbody>';
+        $permissions = $this->select('SELECT * FROM `user_ls_permissions` `p` LEFT JOIN `user_rel_permissions` `r` ON `p`.`permission_id`=`r`.`permission_id` LEFT JOIN `user_ls_groups` `g` ON `r`.`group_id`=`g`.`group_id`;');
+        foreach ($permissions as $p) {
+            $this->view .= '<tr><td>' . "{$p->permission_id}</td><td>{$p->function}</td><td>{$p->group_id}</td><td>{$p->group_name}" . '<td></tr>';
         }
-        $this->view .= '</div><div id="tabs-2">';
-        $gs = $this->select('SELECT * FROM `user_ls_groups`;');
-        foreach ($gs as $g) {
-            $this->view .= serialize($g) . '<br>';
+        $this->view .= '</tbody></table></div><div id="tabs-2">
+        <table><thead><th>group_id</th><th>group_name</th></thead><tbody>';
+        $groups = $this->select('SELECT * FROM `user_ls_groups`;');
+        foreach ($groups as $g) {
+            $this->view .= '<tr><td>' . "{$g->group_id}</td><td>{$g->group_name}" . '<td></tr>';
         }
-        $this->view .= '</div><div id="tabs-3">';
-        $us = $this->select('SELECT * FROM `user_ls_users` `u`
+        $this->view .= '</tbody></table></div><div id="tabs-3">
+        <table><thead><th>user_id</th><th>username</th><th>email</th><th>group_id</th><th>group_name</th></thead><tbody>';
+        $users = $this->select('SELECT * FROM `user_ls_users` `u`
             LEFT JOIN `user_rel_groups` `r` ON `u`.`user_id`=`r`.`user_id`
             LEFT JOIN `user_ls_groups` `g` ON `r`.`group_id`=`g`.`group_id`;');
-        foreach ($us as $u) {
-            $this->view .= serialize($u) . '<br>';
+        foreach ($users as $u) {
+            $this->view .= '<tr><td>' . "{$u->user_id}</td><td>{$u->username}</td><td>{$u->email}</td><td>{$u->group_id}</td><td>{$u->group_name}" . '<td></tr>';
         }
-        $this->view .= '</div><div id="tabs-4">';
-        $as = $this->select('SELECT * FROM `user_view_permissions`;');
-        foreach ($as as $a) {
+        $this->view .= '</tbody></table></div><div id="tabs-4">';
+        $all = $this->select('SELECT * FROM `user_view_permissions`;');
+        foreach ($all as $a) {
             $this->view .= serialize($a) . '<br>';
         }
-        $this->view .= '</div></div><script>$( function() {$( "#tabs" ).tabs();} );</script>';
+        $this->view .= '</div></div><script>$(function(){$("#tabs").tabs();});</script>';
         $this->display();
     }
 
