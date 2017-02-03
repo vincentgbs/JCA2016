19ca76b6546dc1a9055b70e92c77754f671772fb
Vincent Hu <vincenthu@crescagroup.com>
2017-02-03 14:13:36 -0500

    Working to add user permissions

--- framework/controller/frameworkController.php
+++ framework/controller/frameworkController.php
@@ -18,7 +18,7 @@ class frameworkController extends controller {
         if ($this->db->connect_errno) {
             exit('500 Internal Server Error: Database connection error');
         }
-        $this->view = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="author" content=""><meta name="keywords" content=""><meta name="description" content=""><meta name="viewport" content="width=device-width, initial-scale=1"><title>User Management Module</title><link rel="stylesheet" type="text/css" href="/css/library/bootstrap.min.css"><link rel="stylesheet" type="text/css" href="/css/library/jquery-ui.min.css"><link rel="stylesheet" type="text/css" href="/css/library/1.10.12.dataTables.min.css"><link rel="stylesheet" type="text/css" href="/css/style.css"><script type="text/javascript" src="/js/library/jquery-3.1.1.min.js"></script><script type="text/javascript" src="/js/library/sha256.js"></script><script type="text/javascript" src="/js/library/jquery-ui.min.js"></script><script type="text/javascript" src="/js/vanilla.js"></script><script type="text/javascript" src="/js/library/1.10.12.dataTables.min.js"></script></head>';
+        $this->view = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1"><title>User Management Module</title><link rel="stylesheet" type="text/css" href="/css/library/bootstrap.min.css"><link rel="stylesheet" type="text/css" href="/css/library/jquery-ui.min.css"><link rel="stylesheet" type="text/css" href="/css/library/1.10.12.dataTables.min.css"><link rel="stylesheet" type="text/css" href="/css/style.css"><script type="text/javascript" src="/js/library/jquery-3.1.1.min.js"></script><script type="text/javascript" src="/js/library/sha256.js"></script><script type="text/javascript" src="/js/library/jquery-ui.min.js"></script><script type="text/javascript" src="/js/vanilla.js"></script><script type="text/javascript" src="/js/library/1.10.12.dataTables.min.js"></script></head>';
     }
 
     public function permissions()
@@ -26,33 +26,36 @@ class frameworkController extends controller {
         if (isset($_POST['permission'])) {
             //
         }
-        $this->view .= '<div id="tabs"><ul><li><a href="#tabs-4">All</a></li><li><a href="#tabs-1">Permissions</a></li><li><a href="#tabs-2">Groups</a></li><li><a href="#tabs-3">Users</a></li></ul>';
-        $this->view .= '<div id="tabs-1">
-        <table><thead><th>permission_id</th><th>function</th><th>group_id</th><th>group_name</th></thead><tbody>';
-        $permissions = $this->select('SELECT * FROM `user_ls_permissions` `p` LEFT JOIN `user_rel_permissions` `r` ON `p`.`permission_id`=`r`.`permission_id` LEFT JOIN `user_ls_groups` `g` ON `r`.`group_id`=`g`.`group_id`;');
-        foreach ($permissions as $p) {
-            $this->view .= '<tr><td>' . "{$p->permission_id}</td><td>{$p->function}</td><td>{$p->group_id}</td><td>{$p->group_name}" . '<td></tr>';
-        }
-        $this->view .= '</tbody></table></div><div id="tabs-2">
-        <table><thead><th>group_id</th><th>group_name</th></thead><tbody>';
-        $groups = $this->select('SELECT * FROM `user_ls_groups`;');
-        foreach ($groups as $g) {
-            $this->view .= '<tr><td>' . "{$g->group_id}</td><td>{$g->group_name}" . '<td></tr>';
-        }
-        $this->view .= '</tbody></table></div><div id="tabs-3">
-        <table><thead><th>user_id</th><th>username</th><th>email</th><th>group_id</th><th>group_name</th></thead><tbody>';
-        $users = $this->select('SELECT * FROM `user_ls_users` `u`
-            LEFT JOIN `user_rel_groups` `r` ON `u`.`user_id`=`r`.`user_id`
-            LEFT JOIN `user_ls_groups` `g` ON `r`.`group_id`=`g`.`group_id`;');
-        foreach ($users as $u) {
-            $this->view .= '<tr><td>' . "{$u->user_id}</td><td>{$u->username}</td><td>{$u->email}</td><td>{$u->group_id}</td><td>{$u->group_name}" . '<td></tr>';
-        }
-        $this->view .= '</tbody></table></div><div id="tabs-4">';
+        $this->view .= '<style>td {min-width:10%;}</style><div>
+        username: <input type="text" id="username">
+        group_name: <input type="text" id="group_name">
+        function: <input type="text" id="function">
+        <button>Add/Update</button>
+        </div><hr>
+        <table id="permissions"><thead><tr>
+            <th>username</th>
+            <th>email</th>
+            <th>group_id</th>
+            <th>group_name</th>
+            <th>permission_id</th>
+            <th>function</th>
+            <th>delete</th>
+            </tr></thead><tbody>';
         $all = $this->select('SELECT * FROM `user_view_permissions`;');
         foreach ($all as $a) {
-            $this->view .= serialize($a) . '<br>';
+            $this->view .= '<tr>
+                <td>' . "{$a->username}</td>
+                <td>{$a->email}</td>
+                <td>{$a->group_id}</td>
+                <td>{$a->group_name}</td>
+                <td>{$a->permission_id}</td>
+                <td>{$a->function}</td>
+                <td>{$a->user_id} | {$a->group_id}" . '<td>
+            </tr>';
         }
-        $this->view .= '</div></div><script>$(function(){$("#tabs").tabs();});</script>';
+        $this->view .= '</tbody>
+            </table>
+        <script></script>';
         $this->display();
     }
 
