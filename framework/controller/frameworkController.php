6efbe2cfffa14387af8effd8013f07a4621bcc71
Vincent Hu <vincenthu@crescagroup.com>
2017-02-03 11:04:34 -0500

    Working to add user permissions

--- framework/controller/frameworkController.php
+++ framework/controller/frameworkController.php
@@ -26,21 +26,44 @@ class frameworkController extends controller {
         if (isset($_POST['permission'])) {
             //
         }
-        $this->view .= 'Add permission: <input type="text"><br>';
-        $this->view .= 'Add permission to group: <input type="text"><br>';
-        $this->view .= 'Add group: <input type="text"><br>';
-        $this->view .= 'Add user to group: <input type="text"><br>';
-
-        $permissions = $this->select('SELECT * FROM user_view_permissions;');
-        $this->view .= '<pre>';
-        foreach ($permissions as $permission) {
-            $this->view .= serialize($permission) . '<br>';
+        $this->view .= '<div id="tabs"><ul><li><a href="#tabs-1">Permissions</a></li><li><a href="#tabs-2">Groups</a></li><li><a href="#tabs-3">Users</a></li><li><a href="#tabs-4">All</a></li></ul>';
+        $this->view .= '<div id="tabs-1">';
+        $ps = $this->select('SELECT * FROM `user_ls_permissions` `p`
+            LEFT JOIN `user_rel_permissions` `r` ON `p`.`permission_id`=`r`.`permission_id`
+            LEFT JOIN `user_ls_groups` `g` ON `r`.`group_id`=`g`.`group_id`;');
+        foreach ($ps as $p) {
+            $this->view .= serialize($p) . '<br>';
         }
-        $this->view .= '</pre>';
-        echo $this->view;
+        $this->view .= '</div><div id="tabs-2">';
+        $gs = $this->select('SELECT * FROM `user_ls_groups`;');
+        foreach ($gs as $g) {
+            $this->view .= serialize($g) . '<br>';
+        }
+        $this->view .= '</div><div id="tabs-3">';
+        $us = $this->select('SELECT * FROM `user_ls_users` `u`
+            LEFT JOIN `user_rel_groups` `r` ON `u`.`user_id`=`r`.`user_id`
+            LEFT JOIN `user_ls_groups` `g` ON `r`.`group_id`=`g`.`group_id`;');
+        foreach ($us as $u) {
+            $this->view .= serialize($u) . '<br>';
+        }
+        $this->view .= '</div><div id="tabs-4">';
+        $as = $this->select('SELECT * FROM `user_view_permissions`;');
+        foreach ($as as $a) {
+            $this->view .= serialize($a) . '<br>';
+        }
+        $this->view .= '</div></div><script>$( function() {$( "#tabs" ).tabs();} );</script>';
+        $this->display();
     }
 
 
+
+
+
+    private function display()
+    {
+        $this->view .= '</body> <!-- </body class="container"> --></html> <!-- </html lang="en"> -->';
+        echo $this->view;
+    }
 
     private function select($query)
     {
